d5e0e89637420378651c7e82d9cadad0f1243ec4
Sergey <tsv_mail@mail.ru>
2019-10-24 00:06:07 +0300

    huffman decode new version completed

--- src/huffman_code/decoding/Main.java
+++ src/huffman_code/decoding/Main.java
@@ -8,34 +8,29 @@ import java.util.Map;
 
 public class Main {
 
-    private String encodedLine;
-    int qty;
-    int length;
-    Map<String,String> codeTable;
-    String[] lines;
-    String part;
-    StringBuilder result = new StringBuilder();
-    StringBuilder temp = new StringBuilder();
+    private StringBuilder result = new StringBuilder();
+    private StringBuilder temp = new StringBuilder();
 
     private void run() {
 
         try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
 
             String[] header = br.readLine().split(" ");
-            qty = Integer.parseInt(header[0]);
-            length = Integer.parseInt(header[1]);
-            codeTable = new HashMap<>(length);
+            int qty = Integer.parseInt(header[0]);
+            int length = Integer.parseInt(header[1]);
+            Map<String, String> codeTable = new HashMap<>(length);
 
             for (int i = 0; i < qty; i++) {
-                lines = br.readLine().split(": ");
+                String[] lines = br.readLine().split(": ");
                 codeTable.put(lines[1], lines[0]);
             }
 
-            encodedLine = br.readLine();
+            String encodedLine = br.readLine();
 
             for (int i = 0; i < length; i++) {
 
                 temp.append(encodedLine.charAt(i));
+                String part;
                 if ((part = codeTable.get(temp.toString())) != null) {
                     result.append(part);
                     temp.setLength(0);

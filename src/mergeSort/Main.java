741bb625afcdc76693f65f531268244843257efe
Sergey <tsv_mail@mail.ru>
2019-11-09 02:00:01 +0300

    mergeSort task added

---
+++ src/mergeSort/Main.java
@@ -0,0 +1,58 @@
+package mergeSort;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+    private void mergeSort(int[] src, int[] dst, int start, int end) {
+
+        if (end - start < 2) {
+            return;
+        }
+        if (end - start == 2) {
+            if (dst[start] > dst[start + 1]) {
+                int tmp = dst[start];
+                dst[start] = dst[start + 1];
+                dst[start + 1] = tmp;
+            }
+            return;
+        }
+        int mid = (start + end) / 2;
+        mergeSort(dst, src, start, mid);
+        mergeSort(dst, src, mid, end);
+
+        int i = start;
+        int j = mid;
+        int k = start;
+
+        while (k < end) {
+            if (j >= end || (i < mid && src[i] < src[j])) {
+                dst[k] = src[i];
+                i++;
+            } else {
+                dst[k] = src[j];
+                j++;
+            }
+            k++;
+        }
+    }
+
+    private void run() {
+        try(BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+            int size = Integer.parseInt(br.readLine());
+            int[] src = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+            int[] dst = Arrays.copyOf(src, size);
+            mergeSort(dst, src,0, size);
+            Arrays.stream(src).forEach(i -> System.out.print(i + " "));
+            System.err.println();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public static void main(String[] args) {
+
+        new Main().run();
+    }
+}

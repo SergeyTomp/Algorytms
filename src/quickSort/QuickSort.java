4db17a364b4811a1da04c006ec9b62cde1e269b6
Sergey_Tomp <tsv.091265@gmail.com>
2019-11-15 16:39:06 +0300

    работает, но валится по времени на тестах

--- src/quickSort/QuickSort.java
+++ src/quickSort/QuickSort.java
@@ -1,10 +1,9 @@
 package quickSort;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.Objects;
-
-import static quickSort.QuickSort.Side.LEFT;
-import static quickSort.QuickSort.Side.RIGHT;
 
 public class QuickSort {
 
@@ -24,38 +23,19 @@ public class QuickSort {
 
         @Override
         public int compareTo(Point o) {
-//            return this.x == o.x ? - o.side.compareTo(this.side) : this.x - o.x;
-            return this.x - o.x;
+            return this.x == o.x ? - o.side.compareTo(this.side) : this.x - o.x;
         }
-
-//        @Override
-//        public String toString() {
-//            return side.name() + " " + x;
-//        }
 
         @Override
         public String toString() {
-            return String.valueOf(x);
+            return side.name() + " " + x;
         }
-
-//        @Override
-//        public boolean equals(Object o) {
-//            if (this == o) return true;
-//            if (o == null || getClass() != o.getClass()) return false;
-//            Point point = (Point) o;
-//            return x == point.x &&
-//                    side == point.side;
-//        }
     }
 
     private void swap(Point[] array, int j, int i) {
-        System.err.println();
-        Arrays.stream(array).forEach(p -> System.err.print(p.x + " "));
         Point tmp = array[j];
         array[j] = array[i];
         array[i] = tmp;
-        System.err.println();
-        Arrays.stream(array).forEach(p -> System.err.print(p.x + " "));
     }
 
     private int[] partition_3(Point[] array, int left, int right) {
@@ -65,9 +45,11 @@ public class QuickSort {
         for (int i = left + 1; i <= right; i++) {
             if (array[i].compareTo(base) < 0) {
                 j += 1;
-                swap(array, j, i);
                 k += 1;
-                swap(array, k, i);
+                swap(array, j, i);
+                if (k > j) {
+                    swap(array, k, i);
+                }
             } else if (array[i].compareTo(base) == 0) {
                 k += 1;
                 swap(array, k, i);
@@ -76,20 +58,6 @@ public class QuickSort {
         swap(array, left, j);
         j--;
         return new int[]{j, k};
-    }
-
-    private int partition_2(Point[] array, int left, int right) {
-
-        Point base = array[left];
-        int j = left;
-        for (int i = left + 1; i <= right; i++) {
-            if (array[i].compareTo(base) <= 0) {
-                j += 1;
-                swap(array, j, i);
-            }
-            swap(array, left, j);
-        }
-        return j;
     }
 
     private void quickSort(Point [] array, int left, int right) {
@@ -105,26 +73,61 @@ public class QuickSort {
     private int search(Point [] array, int x) {
         int count = 0;
 
+        for (Point p : array) {
+
+            if (p.x > x) {
+                return count;
+            }
+
+            if (p.side == Side.LEFT) {
+                count++;
+            } else if (p.x < x && p.side == Side.RIGHT) {
+                count--;
+            }
+        }
         return count;
     }
 
     private void run() {
-        Point [] array = new Point[]{
-                new Point(-1, LEFT),
-                new Point(4, LEFT),
-                new Point(2, LEFT),
-                new Point(3, LEFT),
-                new Point(4, RIGHT),
-                new Point(5, LEFT),
-                new Point(-1, LEFT),
-                new Point(-1, LEFT)};
-//        System.err.print(Arrays.toString(array));
-        Arrays.stream(array).forEach(p -> System.err.print(p.x + " "));
-        System.err.println();
-        quickSort(array, 0, array.length - 1);
-//        System.err.print(Arrays.toString(array));
-        Arrays.stream(array).forEach(p -> System.err.print(p.x + " "));
 
+        try(BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+
+            String[] qty = br.readLine().split(" ");
+
+            final int linesQty = Integer.parseInt(qty[0]);
+            final Point[] linePoints = new Point[linesQty * 2];
+
+            final int testPointsQty= Integer.parseInt(qty[1]);
+            final int[] testPoints = new int[testPointsQty];
+
+            int linePointIndex = 0;
+            for (int i = 0; i < linesQty; i++) {
+
+                String[] lp = br.readLine().split(" ");
+                linePoints[linePointIndex] = new Point(Integer.parseInt(lp[0]), Side.LEFT);
+                linePointIndex++;
+                linePoints[linePointIndex] = new Point(Integer.parseInt(lp[1]), Side.RIGHT);
+                linePointIndex++;
+            }
+
+            String[] tp = br.readLine().split(" ");
+
+            for (int i = 0; i < testPoints.length; i++) {
+                testPoints[i] = Integer.parseInt(tp[i]);
+            }
+
+            quickSort(linePoints, 0, linePoints.length - 1);
+
+
+
+            StringBuilder out = new StringBuilder();
+            Arrays.stream(testPoints).forEach(p -> out.append(search(linePoints, p)).append(" "));
+            System.out.println(out.toString());
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public static void main(String[] args) {

8fd708b31f0256e300918222733438187cd9d549
Sergey_Tomp <tsv.091265@gmail.com>
2019-11-18 11:14:25 +0300

    QuickSort прошла тесты, затык по превышению времени был в необходимости рандомного выбора опорного элемента в partition.

--- src/quickSort/Main.java
+++ src/quickSort/Main.java
@@ -7,7 +7,7 @@ import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.Random;
 
-public class QuickSort {
+public class Main {
 
     enum Side {
 
@@ -141,6 +141,6 @@ public class QuickSort {
     }
 
     public static void main(String[] args) {
-        new QuickSort().run();
+        new Main().run();
     }
 }

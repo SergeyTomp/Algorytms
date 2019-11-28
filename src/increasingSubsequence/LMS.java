2f77324ffa24da454692bfc3be59d886652637ef
Sergey_Tomp <tsv.091265@gmail.com>
2019-11-28 17:30:08 +0300

    Наибольшая последовательнократная подпоследовательность - прошло тест. Без комментариев ни за что бы не догадался, что при отсутствии хотя бы одного элемента, делящегося на предыдущий, в т.ч. если в массиве вообще один элемент, правильный ответ == 1. Как-то не вяжется с пониманием длины такой последовательности.

--- src/increasingSubsequence/LMS.java
+++ src/increasingSubsequence/LMS.java
@@ -9,7 +9,7 @@ public class LMS {
         int[] sequence = new int[size];
 
         for (int i = 0; i < size; i++) {
-            sequence[i] = 0;
+            sequence[i] = 1;
             for (int j = 0; j < i; j++) {
                 if (array[i] % array[j] == 0 && sequence[j] + 1 > sequence[i]) {
                     sequence[i] = sequence[j] + 1;
@@ -21,10 +21,10 @@ public class LMS {
 
         System.out.println(Arrays.toString(sequence));
 
-        int[] out = new int[len + 1];
+        int[] out = new int[len];
         StringBuilder sb = new StringBuilder(out.length * 2);
 
-        if (len > 0) {
+        if (len > 1) {
 
             int prev_a = -1;
             int prev_s = len;
@@ -41,11 +41,11 @@ public class LMS {
                     out_i++;
                 }
             }
-            System.out.println("length = " + (len + 1));
+            System.out.println("length = " + (len));
             Arrays.stream(out).sorted().forEach(e -> sb.append(e).append(" "));
             System.out.println("sequence: " + sb.toString());
         } else {
-            System.out.println("length = " + 0);
+            System.out.println("length = " + 1);
             System.out.println("sequence: empty");
         }
 
@@ -56,11 +56,11 @@ public class LMS {
     }
 
     public static void main(String[] args) {
-//        int array[] = { 2, 5, 3, 7, 11, 8, 10, 13, 6 };
+        int array[] = { 2, 5, 3, 7, 11, 8, 10, 13, 6 };
 //        int array[] = { 5, 10, 6, 12, 3, 24, 7, 8};
-//        int array[] = { 4, 4, 4, 4, 4, 4, 4, 3};
+//        int array[] = { 4, 4, 4, 4, 4, 4, 4, 4};
 //        int array[] = { 10, 9, 8, 7, 6, 5, 4, 20};
-        int array[] = { 3, 6, 7, 12};
+//        int array[] = { 3, 6, 7, 12};
         int n = array.length;
         new LMS().run(array, n);
     }

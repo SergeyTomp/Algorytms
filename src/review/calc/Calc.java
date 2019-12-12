fa8e7c48298805b000966d8e237d9e1a2ab8c115
Sergey <tsv_mail@mail.ru>
2019-12-13 01:36:19 +0300

    заключительные задачи - калькулятор в работе, вроде считает количество операция, добавил запись истории, доработать вывод

--- src/review/calc/Calc.java
+++ src/review/calc/Calc.java
@@ -1,9 +1,7 @@
 package review.calc;
 
 import java.io.*;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Calc {
     private class FastScanner {
@@ -33,16 +31,33 @@ public class Calc {
 
         int[]oper = new int[n + 1];
         oper[0] = -1;
-        List<List<Integer>> path = new ArrayList<>();
+        int[]prev = new int[n + 1];
+        prev[1] = 1;
         int div_2, div_3;
         for (int i = 1; i < n + 1; i++) {
 
             div_2 = i % 2 == 0 ? oper[i / 2] + 1: Integer.MAX_VALUE;
             div_3 = i % 3 == 0 ? oper[i / 3] + 1: Integer.MAX_VALUE;
             oper[i] = Math.min(Math.min(div_3, div_2), oper[i - 1] + 1);
+
+            if (oper[i] == div_2) {
+                prev[i] = i / 2;
+            } else if (oper[i] == div_3) {
+                prev[i] = i / 3;
+            } else {
+                prev[i] = i - 1;
+            }
         }
 
         System.err.println(oper[n]);
+        System.err.println(Arrays.toString(prev));
+
+        int prv = n;
+        System.err.print(n + " ");
+        while (prv != 1) {
+            System.err.print(prev[prv] + " ");
+            prv = prev[prv];
+        }
     }
 
     private void run() throws FileNotFoundException {
